354ad29ffbe0ff3f72a9aa545abffd60b5502ed8
Abasse MAIGA <ma.maiga@odc.local>
2022-08-19 10:59:21 +0000

    premier commit

---
+++ src/test/java/com/example/API_Tirage/ApiTirageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API_Tirage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTirageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
